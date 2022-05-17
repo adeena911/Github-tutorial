40022adf62629a05086261ec6bfa8f09e5c4afd5
Adeena <adeena@MacBook-Air-Adeena.local>
2022-05-17 19:18:32 +0600

    added three files

---
+++ backend.py
@@ -0,0 +1,19 @@
+'''  Github - система / Github - service 
+     git - система контроля версий для отслеживания изменений в коде имеет древовидную структуру
+     github - это сервис для хостинга для версий 
+'''
+
+# Repository - container which saves code 
+# Commit - a screenshoot of state, we can turnback to them whenever we want 
+
+# main branch --> initial state 
+
+# pull request --> asking someone to merge the branches 
+# merge ---> adding two branches
+
+# git --version --> shows your version of git
+# git --help --> helps with git commands 
+# git init --> initializes a git repository
+# git status --> shows current state of our project
+
+# git add <filename> or . --> adds changes to git 
